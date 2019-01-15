e3b38369b8a8baf91c97dea4c6de32dbff5c81ad
victorx716 <victorliu716@gmail.com>
2019-01-14 20:58:16 -0800

    Added initial code for chatterbox client

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -17,12 +17,12 @@ var App = {
 
   },
 
-  fetch: function(callback = ()=>{}) {
+  fetch: function(callback = (ele)=>{ele}) {
     Parse.readAll((data) => {
       // examine the response from the server request:
       console.log(data);
 
-      callback();
+      callback(data);
     });
   },
 
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,37 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    App.fetch((data) = function() {
+      data.results.map(function(ele) {
+         MessagesView.render(MessageView.render(ele));
+      })
+    });
+  },
+//look into when initialize is being called
+
+  render: function(message) {
+    // $.getJSON(`http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`, function(data) {
+    //   console.log('got data', data);
+    //   $('body').append(data);
+    // })
+
+     MessagesView.$chats.append(message);
+    // $.ajax({
+    //   url: "http://parse.sfm6.hackreactor.com/chatterbox/classes/messages",
+    //   dataType: "json",
+    //   success: 
+    //   function(message) {
+    //   var cool = [];
+    //   for (var i = 0 ; i < message.length; i++) {
+    //     cool[i] = message.results[i].text;
+    //    }
+    //   }
+    // })
+    // var compiled = _.template("hello: <%= message %>");
   },
 
-  render: function() {
+  renderMessage: function(message) {
+    MessagesView.$chats.append(message);
   }
 
 };
